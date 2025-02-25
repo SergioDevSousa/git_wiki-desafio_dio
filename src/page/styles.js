cdc519397ac43dd6d9afa6da6180c15656fbf1a0
SergioDevSousa <oxentesergiosousa76@outlook.com>
2025-02-24 23:46:22 -0300

    update inicial

---
+++ src/page/styles.js
@@ -0,0 +1,10 @@
+import styles from "styled-components";
+
+export const Container = styles.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    margin: 20px 0 0 0;
+`;
