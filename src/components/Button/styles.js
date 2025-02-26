e6a20402067a283fe7091d67c7b0a0519fd9171c
SergioDevSousa <oxentesergiosousa76@outlook.com>
2025-02-25 21:10:52 -0300

    update button

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    background-color: #fafafa;
+    border: 1px solid #fafafa;
+    border-radius: 20px;
+    height: 30px;
+    width: 15%;
+    margin: 20px;
+
+    &:hover {
+        background-color: #fafafa40;
+        cursor: pointer;
+    }
+
+`;
--- src/page/App.js
+++ src/page/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import gitlogo from '../assets/image/github.png';
 import { Container } from './styles';
 import Input from '../components/Input';
+import Button from '../components/Button';
 import ItemRepo from '../components/ItemRepo';
 
 function App() {
+  const [repos, setRepos] = useState ([]);
+
+
+
   return (
     <Container >
         <img src={gitlogo} width={72} height={72} alt="logo github"/>
         <Input />
+        <Button />
         <ItemRepo />
     </Container>
   );
